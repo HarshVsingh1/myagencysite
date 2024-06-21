019cd5ed92d2bf201da05e5e606df807f876ef29
Aish Mishra <aishlunatic001@gmail.com>
2024-06-22 00:58:01 +0530

    swiper

--- src/component/ui/Swiper.jsx
+++ src/component/ui/Swiper.jsx
@@ -7,47 +7,36 @@ import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import 'swiper/css/autoplay';
-import Image1 from '../../assets/ProductDesign/Image1.png'
-import Image2 from '../../assets/ProductDesign/Image2.png'
-import Image3 from '../../assets/ProductDesign/Image3.png'
-import Image4 from '../../assets/ProductDesign/Image4.png'
-import Image5 from '../../assets/ProductDesign/Image5.png'
-import Image6 from '../../assets/ProductDesign/Image6.png'
-import Image7 from '../../assets/ProductDesign/Image7.png'
-import Image8 from '../../assets/ProductDesign/Image8.png'
-import Image9 from '../../assets/ProductDesign/Image9.png'
-import Image10 from '../../assets/ProductDesign/Image10.png'
-import Image11 from '../../assets/ProductDesign/Image11.png'
-import Image12 from '../../assets/ProductDesign/Image12.png'
-import Image13 from '../../assets/ProductDesign/Image13.png'
-import Image14 from '../../assets/ProductDesign/Image14.png'
-import Image15 from '../../assets/ProductDesign/Image15.png'
-
-import './Swiper.css';
 
 // Import required Swiper modules
 import { EffectCoverflow, Pagination, Autoplay } from 'swiper/modules';
 
+// Import images
+import Image1 from '../../assets/ProductDesign/Image1.png';
+import Image2 from '../../assets/ProductDesign/Image2.png';
+import Image3 from '../../assets/ProductDesign/Image3.png';
+import Image4 from '../../assets/ProductDesign/Image4.png';
+import Image5 from '../../assets/ProductDesign/Image5.png';
+import Image6 from '../../assets/ProductDesign/Image6.png';
+import Image7 from '../../assets/ProductDesign/Image7.png';
+import Image8 from '../../assets/ProductDesign/Image8.png';
+import Image9 from '../../assets/ProductDesign/Image9.png';
+import Image10 from '../../assets/ProductDesign/Image10.png';
+import Image11 from '../../assets/ProductDesign/Image11.png';
+import Image12 from '../../assets/ProductDesign/Image12.png';
+import Image13 from '../../assets/ProductDesign/Image13.png';
+import Image14 from '../../assets/ProductDesign/Image14.png';
+import Image15 from '../../assets/ProductDesign/Image15.png';
+
+import './Swiper.css';
+
 // Array of image paths
 const images = [
-  Image1 ,
-  Image2 ,
-  Image3 ,
-  Image4 ,
-  Image5 ,
-  Image6 ,
-  Image7 ,
-  Image8 ,
-  Image9 ,
-  Image10 ,
-  Image11 ,
-  Image12 ,
-  Image13 ,
-  Image14 ,
-  Image15 
+  Image1, Image2, Image3, Image4, Image5, Image6, Image7, Image8,
+  Image9, Image10, Image11, Image12, Image13, Image14, Image15
 ];
 
-function SwiperComponent() {
+const SwiperComponent = () => {
   return (
     <div>
       <Swiper
@@ -57,8 +46,8 @@ function SwiperComponent() {
         slidesPerView={'auto'}
         coverflowEffect={{
           rotate: 50,
-          stretch: 1,
-          depth: 100,
+          stretch: 0,
+          depth: 300,  // Adjust depth to make center slide appear farther
           modifier: 1,
           slideShadows: true,
         }}
@@ -66,9 +55,9 @@ function SwiperComponent() {
           clickable: true,
         }}
         autoplay={{
-          delay: 3000, // Slide delay in ms
+          delay: 3000,
           disableOnInteraction: false,
-          reverseDirection: false, // Normal sliding direction
+          reverseDirection: false,
         }}
         loop={true}
         modules={[EffectCoverflow, Pagination, Autoplay]}
@@ -76,7 +65,7 @@ function SwiperComponent() {
       >
         {images.map((src, index) => (
           <SwiperSlide key={index}>
-            <img src={src} alt={`Nature ${index + 1}`} />
+            <img src={src} alt={`Product Design ${index + 1}`} />
           </SwiperSlide>
         ))}
       </Swiper>
